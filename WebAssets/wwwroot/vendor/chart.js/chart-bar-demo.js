7ff6c4de48917aac3f42bf51c47ae19d98794b70
rheks <ktsr.andre@gmail.com>
2023-01-06 22:00:26 +0700

    Add 2 report chart

--- WebAssets/wwwroot/vendor/chart.js/chart-bar-demo.js
+++ WebAssets/wwwroot/vendor/chart.js/chart-bar-demo.js
@@ -1,3 +1,39 @@
+let dataEmployees = [];
+let dataTotalBorrowAssets = [];
+let maxFrequency = 0;
+
+$.ajax({
+    "type": "GET",
+    "url": urlBackend + "/BorrowAssets/Request/EmployeesMostFrequentlyBorrowAssets",
+    "contentType": "application/json; charset=utf-8",
+    "dataType": "json",
+    "success": function (result) {
+        var obj = result.data;
+
+        obj.forEach((data, index, arr) => {
+            dataEmployees.push(data.firstName)
+            dataTotalBorrowAssets[index] = data.total_BorrowAssets
+
+            if (maxFrequency < data.total_BorrowAssets) {
+                maxFrequency = data.total_BorrowAssets
+            }
+        })
+
+        if (maxFrequency % 2 != 0) {
+            maxFrequency += 7
+        } else {
+            maxFrequency += 8
+        }
+    },
+    "async": false,
+    "error": function (e) {
+        console.log(e);
+    }
+})
+
+//console.log(dataEmployees[1])
+//console.log(dataTotalBorrowAssets[1])
+
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
@@ -27,128 +63,89 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
-let dataEmployees = [];
-let dataTotalBorrowAssets = [];
-
-//fetch(urlBackend + "/BorrowAssets/Request/EmployeesMostFrequentlyBorrowAssets")
-//    .then(function (response) {
-//        // response is a json string
-//        return response.json(); // convert it to a pure JavaScript object
-//    })
-//    .then(function (result) {
-//        var obj = result.data;
-
-//        obj.forEach((data, index, arr) => {
-//            //console.log(data.nik)
-//            dataEmployees.push(data.nik)
-//        })
-//    })
-//    .catch(function (err) {
-//        console.log(err);
-//    });
-console.log(dataEmployees)
-console.log(["January", "February", "March", "April", "May"])
-
-let dataLabels = $.ajax({
-    "type": "GET",
-    "url": urlBackend + "/BorrowAssets/Request/EmployeesMostFrequentlyBorrowAssets",
-    "contentType": "application/json; charset=utf-8",
-    "dataType": "json",
-    "success": function (result) {
-        var obj = result.data;
-
-        obj.forEach((data, index, arr) => {
-            //console.log(data.nik)
-            dataEmployees.push(toString(data.firstName))
-            dataTotalBorrowAssets.push(data.total_BorrowAssets)
-        })
-    },
-    "error": function (e) {
-        console.log(e);
-    }
+$(window).on("load", function () {
+    // Bar Chart Example
+    var ctx = document.getElementById("myBarChart");
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: dataEmployees,
+            datasets: [{
+                label: "Frequency",
+                backgroundColor: "#4e73df",
+                hoverBackgroundColor: "#2e59d9",
+                borderColor: "#4e73df",
+                data: dataTotalBorrowAssets,
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'month'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 6
+                    },
+                    maxBarThickness: 25,
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: maxFrequency,
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function (value, index, values) {
+                            return number_format(value);
+                        }
+                    },
+                    gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+                callbacks: {
+                    label: function (tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + '';
+                    }
+                }
+            },
+        }
+    });
 })
 
 
-
-// Bar Chart Example
-var ctx = document.getElementById("myBarChart");
-var myBarChart = new Chart(ctx, {
-  type: 'bar',
-    data: {
-    labels: ["January", "February", "March", "April", "May"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
-      hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
-    },
-  }
-});
--- WebAssets/wwwroot/vendor/chart.js/chart-pie-demo.js
+++ WebAssets/wwwroot/vendor/chart.js/chart-pie-demo.js
@@ -1,35 +1,64 @@
+let assetsName = []
+let totalBorrower = []
+
+$.ajax({
+    "type": "GET",
+    "url": urlBackend + "/BorrowAssets/Request/MostFrequentlyBorrowAssets",
+    "contentType": "application/json; charset=utf-8",
+    "dataType": "json",
+    "success": function (result) {
+        var obj = result.data;
+
+        obj.forEach((data, index, arr) => {
+            assetsName.push(data.name)
+            $("#asset-" + index).html(data.name)
+
+            totalBorrower.push(data.total_Borrower)
+        })
+    },
+    "async": false,
+    "error": function (e) {
+        console.log(e);
+    }
+})
+
+//console.log(assetsName)
+//console.log(totalBorrower)
+
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
-});
+$(window).on("load", function () {
+    // Pie Chart Example
+    var ctx = document.getElementById("myPieChart");
+    var myPieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: assetsName,
+            datasets: [{
+                data: totalBorrower,
+                backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 80,
+        },
+    });
+})
